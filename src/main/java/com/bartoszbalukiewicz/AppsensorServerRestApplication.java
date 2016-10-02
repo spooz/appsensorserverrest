a0150a1c2e6fe9204cce0f3f61ae960d9e7515e4
Bartosz Balukiewicz <bartekbalukiewicz@gmail.com>
2016-10-02 12:54:21 +0200

    Initial commit

---
+++ src/main/java/com/bartoszbalukiewicz/AppsensorServerRestApplication.java
@@ -0,0 +1,12 @@
+package com.bartoszbalukiewicz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppsensorServerRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppsensorServerRestApplication.class, args);
+	}
+}
